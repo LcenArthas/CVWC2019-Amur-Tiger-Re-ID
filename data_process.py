58bdfd1b3bc0768bb378a4bb39d3797e61955c26
LoveLetter <48119351+LcenArthas@users.noreply.github.com>
2019-08-31 12:29:02 +0800

    Add files via upload

---
+++ data_process.py
@@ -0,0 +1,11 @@
+from pr_data.tiger_market1501 import First
+from pr_data.tiger_bbox import Second
+from pr_data.make_presudo_pic import Third
+
+if __name__ == '__main__':
+    print("Starting first step.....")
+    First()
+    print("Start second step.....")
+    Second()
+    print("Start third step......")
+    Third()
